d67a331c0ef558c5208135b57b24eae20a0eea2c
dartwlad <wlad792@gmail.com>
2020-11-08 16:39:13 +0200

    feat(3): move to real api gateway

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  products: 'https://2bs26yvgma.execute-api.us-east-1.amazonaws.com/dev'
+  products: 'https://gv7d9a48r7.execute-api.us-east-1.amazonaws.com/dev'
 };
 
 export default API_PATHS;
