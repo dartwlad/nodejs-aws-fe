97e9908955b4c06592c8c4f5b721f20c73c8177a
dartwlad <wlad792@gmail.com>
2020-12-12 15:40:04 +0200

    feat(9): same bff link for both services

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,8 +2,8 @@
 const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://4z0ucc3yn5.execute-api.us-east-1.amazonaws.com/dev',
-  products: 'https://smdlu52cef.execute-api.us-east-1.amazonaws.com/dev',
-  cart: 'http://dartwlad-cart-api.us-east-1.elasticbeanstalk.com/api'
+  products: 'http://dartwlad-bff-api-development.us-east-1.elasticbeanstalk.com',
+  cart: 'http://dartwlad-bff-api-development.us-east-1.elasticbeanstalk.com'
 };
 
 export default API_PATHS;
